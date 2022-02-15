be018c3ec805caa2631dfc10bc7e27b142149a92
Sad Mia <75561973+sadmia@users.noreply.github.com>
2022-02-16 00:17:29 +0600

    Update castom.js

--- js/castom.js
+++ js/castom.js
@@ -20,9 +20,16 @@ function remove() {
 function bakc() {
 	let valueLength = display.value.length;
 	let lastValue = display.value.substring(0, valueLength - 1);
+	let miniLastValue = miniDisplay.value.substring(0, valueLength - 1);
 	display.value = lastValue;
+	miniDisplay.value = miniLastValue;
 	if (display.value == 0) {
 		miniDisplay.value = "";
+	}
+	if (display.value == miniDisplay.value) {
+		
+	} else {
+		miniDisplay.value = display.value;
 	}
 }
 
